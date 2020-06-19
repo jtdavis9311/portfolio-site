282aae498274fffbe27ac532781f0f5bce7bba68
Jordan Davis <jordan@milkshake.studio>
2020-06-19 14:36:17 -0400

    start

---
+++ src/components/Home.vue
@@ -0,0 +1,25 @@
+<template>
+	<div id="home">
+		<h1>Jordan Davis</h1>
+
+		<h2>Projects I have helped build:</h2>
+		
+		<a href="https://www.myclean.com/">MyClean</a>
+		<a href="https://www.olo.com/">Olo</a>
+		<a href="https://timeequities.com/">Time Equities Inc.</a>
+		<a href="https://crunchtime.com/">CrunchTime!</a>
+		<a href="https://proplete.com/">Proplete</a>
+		<a href="https://projectentrepreneur.org/">Project Entrepreneur</a>
+		<a href="https://yellowgoatdesign.com/">Yellow Goat Design</a>
+		<a href="https://chestertravels.com/">Chester</a>
+	</div>
+</template>
+
+<script>
+
+export default {
+	name: 'Home',
+	components: {}
+}
+
+</script>
