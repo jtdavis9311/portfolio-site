f2121b9a06a06a96df999d8aaf7c92a158f84a7b
Jordan Davis <jordan@milkshake.studio>
2020-06-19 17:15:09 -0400

    base styling

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,17 +1,21 @@
 <template>
 	<div id="home">
-		<h1>Jordan Davis</h1>
 
-		<h2>Projects I have helped build:</h2>
+		<div class="text">
+			<h1>Jordan Davis</h1>
+			<p>Front End Engineer, Milkshake Studio</p>
+		</div>
 		
-		<a href="https://www.myclean.com/">MyClean</a>
-		<a href="https://www.olo.com/">Olo</a>
-		<a href="https://timeequities.com/">Time Equities Inc.</a>
-		<a href="https://crunchtime.com/">CrunchTime!</a>
-		<a href="https://proplete.com/">Proplete</a>
-		<a href="https://projectentrepreneur.org/">Project Entrepreneur</a>
-		<a href="https://yellowgoatdesign.com/">Yellow Goat Design</a>
-		<a href="https://chestertravels.com/">Chester</a>
+		<div class="links">
+			<a href="https://www.myclean.com/">MyClean</a>
+			<a href="https://www.olo.com/">Olo</a>
+			<a href="https://timeequities.com/">Time Equities Inc.</a>
+			<a href="https://crunchtime.com/">CrunchTime!</a>
+			<a href="https://proplete.com/">Proplete</a>
+			<a href="https://projectentrepreneur.org/">Project Entrepreneur</a>
+			<a href="https://yellowgoatdesign.com/">Yellow Goat Design</a>
+			<a href="https://chestertravels.com/">Chester</a>
+		</div>
 	</div>
 </template>
 
