deaf7bc452eb8374ce3b100e2c8332947e0165c9
Jordan Davis <jordan@milkshake.studio>
2020-07-10 17:22:19 -0400

    updates

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,7 @@
 <template>
 	<div id="home">
 
-		<div class="text">
+		<div :class="['left', hover]">
 			<h1>Jordan Davis</h1>
 			<p>Front End Engineer, <a href="https://milkshake.studio/">Milkshake Studio</a></p>
 
@@ -11,15 +11,70 @@
 			</div>
 		</div>
 		
-		<div class="links">
-			<a href="https://www.myclean.com/">MyClean</a>
-			<a href="https://www.olo.com/">Olo</a>
-			<a href="https://timeequities.com/">Time Equities Inc.</a>
-			<a href="https://crunchtime.com/">CrunchTime!</a>
-			<a href="https://proplete.com/">Proplete</a>
-			<a href="https://projectentrepreneur.org/">Project Entrepreneur</a>
-			<a href="https://yellowgoatdesign.com/">Yellow Goat Design</a>
-			<a href="https://chestertravels.com/">Chester</a>
+		<div class="right">
+			<a href="https://www.myclean.com/">
+				<p>MyClean</p>
+				<p>MyClean</p>
+				<p>MyClean</p>
+				<p>MyClean</p>
+				<p>MyClean</p>
+				<p>MyClean</p>
+			</a>
+			<a href="https://www.olo.com/">
+				<p>Olo</p>
+				<p>Olo</p>
+				<p>Olo</p>
+				<p>Olo</p>
+				<p>Olo</p>
+				<p>Olo</p>
+				<p>Olo</p>
+			</a>
+			<a href="https://timeequities.com/">
+				<p>Time Equities Inc.</p>
+				<p>Time Equities Inc.</p>
+				<p>Time Equities Inc.</p>
+				<p>Time Equities Inc.</p>
+			</a>
+			<a href="https://crunchtime.com/">
+				<p>CrunchTime!</p>
+				<p>CrunchTime!</p>
+				<p>CrunchTime!</p>
+				<p>CrunchTime!</p>
+			</a>
+			<a href="https://proplete.com/">
+				<p>Proplete</p>
+				<p>Proplete</p>
+				<p>Proplete</p>
+				<p>Proplete</p>
+				<p>Proplete</p>
+				<p>Proplete</p>
+			</a>
+			<a href="https://projectentrepreneur.org/">
+				<p>Project Entrepreneur</p>
+				<p>Project Entrepreneur</p>
+				<p>Project Entrepreneur</p>
+				<p>Project Entrepreneur</p>
+			</a>
+			<a href="https://yellowgoatdesign.com/">
+				<p>Yellow Goat Design</p>
+				<p>Yellow Goat Design</p>
+				<p>Yellow Goat Design</p>
+				<p>Yellow Goat Design</p>
+			</a>
+			<a href="https://chestertravels.com/">
+				<p>Chester</p>
+				<p>Chester</p>
+				<p>Chester</p>
+				<p>Chester</p>
+				<p>Chester</p>
+				<p>Chester</p>
+			</a>
+			<a href="https://iendorsebooker.com/">
+				<p>I Endorse Booker</p>
+				<p>I Endorse Booker</p>
+				<p>I Endorse Booker</p>
+				<p>I Endorse Booker</p>
+			</a>
 		</div>
 	</div>
 </template>
@@ -32,8 +87,8 @@ export default {
 	name: 'Home',
 	components: {
 		Github,
-		Linkedin
-	}
+		Linkedin,
+	},
 }
 
 </script>
