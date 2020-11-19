d7892a117e388fcd88600f520845bc60b6e773f2
Jordan Davis <jordan@milkshake.studio>
2020-11-18 20:02:46 -0500

    simplified

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,7 +3,7 @@
 
 		<div :class="['left', hover]">
 			<h1>Jordan Davis</h1>
-			<p>Front End Engineer, <a href="https://milkshake.studio/">Milkshake Studio</a></p>
+			<p>Front End Engineer, <a class="milkshake" href="https://milkshake.studio/">Milkshake Studio</a></p>
 
 			<div class="links">
 				<a href="https://www.linkedin.com/in/jordantd/"><Github></Github></a>
@@ -14,66 +14,66 @@
 		<div class="right">
 			<a href="https://www.myclean.com/" target="_blank">
 				<p>MyClean</p>
-				<p>MyClean</p>
-				<p>MyClean</p>
-				<p>MyClean</p>
-				<p>MyClean</p>
-				<p>MyClean</p>
+				<!-- <p>MyClean</p> -->
+				<!-- <p>MyClean</p> -->
+				<!-- <p>MyClean</p> -->
+				<!-- <p>MyClean</p> -->
+				<!-- <p>MyClean</p> -->
 			</a>
 			<a href="https://www.olo.com/" target="_blank">
 				<p>Olo</p>
-				<p>Olo</p>
-				<p>Olo</p>
-				<p>Olo</p>
-				<p>Olo</p>
-				<p>Olo</p>
-				<p>Olo</p>
+				<!-- <p>Olo</p> -->
+				<!-- <p>Olo</p> -->
+				<!-- <p>Olo</p> -->
+				<!-- <p>Olo</p> -->
+				<!-- <p>Olo</p> -->
+				<!-- <p>Olo</p> -->
 			</a>
 			<a href="https://timeequities.com/" target="_blank">
 				<p>Time Equities Inc.</p>
-				<p>Time Equities Inc.</p>
-				<p>Time Equities Inc.</p>
-				<p>Time Equities Inc.</p>
+				<!-- <p>Time Equities Inc.</p> -->
+				<!-- <p>Time Equities Inc.</p> -->
+				<!-- <p>Time Equities Inc.</p> -->
 			</a>
 			<a href="https://crunchtime.com/" target="_blank">
 				<p>CrunchTime!</p>
-				<p>CrunchTime!</p>
-				<p>CrunchTime!</p>
-				<p>CrunchTime!</p>
+				<!-- <p>CrunchTime!</p> -->
+				<!-- <p>CrunchTime!</p> -->
+				<!-- <p>CrunchTime!</p> -->
 			</a>
 			<a href="https://proplete.com/" target="_blank">
 				<p>Proplete</p>
-				<p>Proplete</p>
-				<p>Proplete</p>
-				<p>Proplete</p>
-				<p>Proplete</p>
-				<p>Proplete</p>
+				<!-- <p>Proplete</p> -->
+				<!-- <p>Proplete</p> -->
+				<!-- <p>Proplete</p> -->
+				<!-- <p>Proplete</p> -->
+				<!-- <p>Proplete</p> -->
 			</a>
 			<a href="https://projectentrepreneur.org/" target="_blank">
 				<p>Project Entrepreneur</p>
-				<p>Project Entrepreneur</p>
-				<p>Project Entrepreneur</p>
-				<p>Project Entrepreneur</p>
+				<!-- <p>Project Entrepreneur</p> -->
+				<!-- <p>Project Entrepreneur</p> -->
+				<!-- <p>Project Entrepreneur</p> -->
 			</a>
 			<a href="https://yellowgoatdesign.com/" target="_blank">
 				<p>Yellow Goat Design</p>
-				<p>Yellow Goat Design</p>
-				<p>Yellow Goat Design</p>
-				<p>Yellow Goat Design</p>
+				<!-- <p>Yellow Goat Design</p> -->
+				<!-- <p>Yellow Goat Design</p> -->
+				<!-- <p>Yellow Goat Design</p> -->
 			</a>
 			<a href="https://chestertravels.com/" target="_blank">
 				<p>Chester</p>
-				<p>Chester</p>
-				<p>Chester</p>
-				<p>Chester</p>
-				<p>Chester</p>
-				<p>Chester</p>
+				<!-- <p>Chester</p> -->
+				<!-- <p>Chester</p> -->
+				<!-- <p>Chester</p> -->
+				<!-- <p>Chester</p> -->
+				<!-- <p>Chester</p> -->
 			</a>
 			<a href="https://iendorsebooker.com/" target="_blank">
 				<p>I Endorse Booker</p>
-				<p>I Endorse Booker</p>
-				<p>I Endorse Booker</p>
-				<p>I Endorse Booker</p>
+				<!-- <p>I Endorse Booker</p> -->
+				<!-- <p>I Endorse Booker</p> -->
+				<!-- <p>I Endorse Booker</p> -->
 			</a>
 		</div>
 	</div>
