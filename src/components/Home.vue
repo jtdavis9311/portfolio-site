373db5d9c5a5203ae0326e1c9428100645ee5958
Jordan Davis <jordan@milkshake.studio>
2021-01-08 18:04:02 -0500

    responsive updates

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -14,66 +14,30 @@
 		<div class="right">
 			<a href="https://www.myclean.com/" target="_blank">
 				<p>MyClean</p>
-				<!-- <p>MyClean</p> -->
-				<!-- <p>MyClean</p> -->
-				<!-- <p>MyClean</p> -->
-				<!-- <p>MyClean</p> -->
-				<!-- <p>MyClean</p> -->
 			</a>
 			<a href="https://www.olo.com/" target="_blank">
 				<p>Olo</p>
-				<!-- <p>Olo</p> -->
-				<!-- <p>Olo</p> -->
-				<!-- <p>Olo</p> -->
-				<!-- <p>Olo</p> -->
-				<!-- <p>Olo</p> -->
-				<!-- <p>Olo</p> -->
 			</a>
 			<a href="https://timeequities.com/" target="_blank">
 				<p>Time Equities Inc.</p>
-				<!-- <p>Time Equities Inc.</p> -->
-				<!-- <p>Time Equities Inc.</p> -->
-				<!-- <p>Time Equities Inc.</p> -->
 			</a>
 			<a href="https://crunchtime.com/" target="_blank">
 				<p>CrunchTime!</p>
-				<!-- <p>CrunchTime!</p> -->
-				<!-- <p>CrunchTime!</p> -->
-				<!-- <p>CrunchTime!</p> -->
 			</a>
 			<a href="https://proplete.com/" target="_blank">
 				<p>Proplete</p>
-				<!-- <p>Proplete</p> -->
-				<!-- <p>Proplete</p> -->
-				<!-- <p>Proplete</p> -->
-				<!-- <p>Proplete</p> -->
-				<!-- <p>Proplete</p> -->
 			</a>
 			<a href="https://projectentrepreneur.org/" target="_blank">
 				<p>Project Entrepreneur</p>
-				<!-- <p>Project Entrepreneur</p> -->
-				<!-- <p>Project Entrepreneur</p> -->
-				<!-- <p>Project Entrepreneur</p> -->
 			</a>
 			<a href="https://yellowgoatdesign.com/" target="_blank">
 				<p>Yellow Goat Design</p>
-				<!-- <p>Yellow Goat Design</p> -->
-				<!-- <p>Yellow Goat Design</p> -->
-				<!-- <p>Yellow Goat Design</p> -->
 			</a>
 			<a href="https://chestertravels.com/" target="_blank">
 				<p>Chester</p>
-				<!-- <p>Chester</p> -->
-				<!-- <p>Chester</p> -->
-				<!-- <p>Chester</p> -->
-				<!-- <p>Chester</p> -->
-				<!-- <p>Chester</p> -->
 			</a>
 			<a href="https://iendorsebooker.com/" target="_blank">
 				<p>I Endorse Booker</p>
-				<!-- <p>I Endorse Booker</p> -->
-				<!-- <p>I Endorse Booker</p> -->
-				<!-- <p>I Endorse Booker</p> -->
 			</a>
 		</div>
 	</div>
