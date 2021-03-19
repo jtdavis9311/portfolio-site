51495da64722ab86fa53b91638476378a608da73
Jordan Davis <jordan@milkshake.studio>
2021-03-19 19:27:10 -0400

    icons

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -6,58 +6,107 @@
 			<p>Front End Engineer, <a class="milkshake" href="https://milkshake.studio/" target="_blank">Milkshake Studio</a></p>
 
 			<div class="links">
-				<a href="https://github.com/jordantd5" target="_blank"><Github></Github></a>
-				<a href="https://www.linkedin.com/in/jordantd/" target="_blank"><Linkedin></Linkedin></a>
+				<a href="https://github.com/jordantd5" target="_blank"><Github /></a>
+				<a href="https://www.linkedin.com/in/jordantd/" target="_blank"><Linkedin /></a>
 			</div>
 		</div>
 		
 		<div class="right">
 			<a href="https://chestertravels.com/" target="_blank">
 				<p>Chester</p>
+				<div class="technologies">
+					<Wordpress />
+				</div>
 			</a>
 			<a href="https://crunchtime.com/" target="_blank">
 				<p>CrunchTime!</p>
+				<div class="technologies">
+					<Craft />
+				</div>
 			</a>
 			<a href="https://iendorsebooker.com/" target="_blank">
 				<p>I Endorse Booker</p>
+				<div class="technologies">
+					<React />
+				</div>
 			</a>
 			<a href="https://www.myclean.com/" target="_blank">
 				<p>MyClean</p>
+				<div class="technologies">
+					<Vue />
+				</div>
 			</a>
 			<a href="https://www.olo.com/" target="_blank">
 				<p>Olo</p>
+				<div class="technologies">
+					<Craft /><GraphQL />
+				</div>
 			</a>
 			<a href="https://populum.com/" target="_blank">
 				<p>Populum</p>
+				<div class="technologies">
+					<Shopify /><GraphQL /><Contentful />
+				</div>
 			</a>
 			<a href="https://projectentrepreneur.org/" target="_blank">
 				<p>Project Entrepreneur</p>
+				<div class="technologies">
+					<Wordpress />
+				</div>
 			</a>
 			<a href="https://proplete.com/" target="_blank">
 				<p>Proplete</p>
+				<div class="technologies">
+					<Craft />
+				</div>
 			</a>
 			<a href="https://timeequities.com/" target="_blank">
 				<p>Time Equities Inc.</p>
+				<div class="technologies">
+					<Craft />
+				</div>
 			</a>
 			<a href="https://voymedia.com/" target="_blank">
 				<p>Voy Media</p>
+				<div class="technologies">
+					<Wordpress />
+				</div>
 			</a>
 			<a href="https://yellowgoatdesign.com/" target="_blank">
 				<p>Yellow Goat Design</p>
+				<div class="technologies">
+					<Craft />
+				</div>
 			</a>
 		</div>
 	</div>
 </template>
 
 <script>
-import Github from './github';
-import Linkedin from './linkedin';
+import Github from './svgs/github';
+import Linkedin from './svgs/linkedin';
+import React from './svgs/react';
+import Vue from './svgs/vue';
+import GraphQL from './svgs/graphql';
+import Contentful from './svgs/contentful';
+import Craft from './svgs/craft';
+import Shopify from './svgs/shopify';
+import Wordpress from './svgs/wordpress';
+// import Figma from './svgs/figms';
 
 export default {
 	name: 'Home',
 	components: {
 		Github,
 		Linkedin,
+		React,
+		Vue,
+		GraphQL,
+		Contentful,
+		Craft,
+		Shopify,
+		Wordpress,
+		// Figma,
 	},
 }
 
---
+++ src/components/svgs/craft.vue
@@ -0,0 +1,11 @@
+<template>
+	<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 53.4 60.1" style="enable-background:new 0 0 53.4 60.1;" xml:space="preserve" width="53.4" height="60.1">
+		<path d="M47.8,0H5.6C2.5,0,0,2.5,0,5.6v42.2c0,3.1,2.5,5.6,5.6,5.6h42.2c3.1,0,5.6-2.5,5.6-5.6V5.6
+			C53.4,2.5,50.9,0,47.8,0 M26.6,32.6c1.8,0,3.6-0.7,5.4-2.3l2.5,2.9c-2.6,2.1-5.6,3.3-8.6,3.3c-5.9,0-9.7-3.9-8.8-9.5
+			S23,17.5,29,17.5c2.9,0,5.6,1.1,7.4,3.2l-3.5,2.9c-1-1.3-2.7-2.2-4.6-2.2c-3.6,0-6.3,2.3-6.9,5.6C21,30.2,23,32.6,26.6,32.6" fill="#E5422B"></path>
+	</svg>
+</template>
+
+<script>
+export default { name: 'craft' }
+</script>
